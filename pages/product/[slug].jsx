59023f934a07e65f0a09f2abba20b9f97dff444f
David Silveira <daviewave@gmail.com>
2023-06-02 20:05:32 -0400

    added a similar products section beneath the product clicked on

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -77,6 +77,17 @@ const ProductDetails = ({ product, products }) => {
           </div>
         </div>
       </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products?.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
