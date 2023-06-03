92d73cd53379bfadf0c9a60efdacb7980426cada
David Silveira <daviewave@gmail.com>
2023-06-02 21:16:00 -0400

    added context file for handling the products amount in the product details page

---
+++ context/StateContext.jsx
@@ -0,0 +1,81 @@
+/* NOTE: I want to ask chatGPT how this same file would look in:
+    1. TypeScript
+    2. using the hook useReducer
+*/
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  const increaseQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decreaseQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
+    });
+  };
+
+  const addToCart = (product, quantity) => {
+    const checkIfProductIsAlreadyInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkIfProductIsAlreadyInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty}x ${product.name} added to cart`);
+  };
+
+  const removeFromCart = () => {};
+
+  const clearCart = () => {};
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        increaseQty,
+        decreaseQty,
+        addToCart,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export default function useStateContext() {
+  return useContext(Context);
+}
