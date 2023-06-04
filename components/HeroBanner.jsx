cfba33e10edfdf051a741970f058994155dc4468
David Silveira <daviewave@gmail.com>
2023-06-03 20:11:02 -0400

    restyled the hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,29 +3,29 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({
+  heroBanner: { smallText, midText, image, largeText1, buttonText, desc },
+}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
+        {/* <p className="beats-solo">{smallText}</p> */}
+        <p className="beats-solo">{desc}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
 
         <img
-          src={urlFor(heroBanner.image)}
+          src={urlFor(image)}
           alt="headphones"
           className="hero-banner-image"
         />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">{heroBanner.buttonText}</button>
-          </Link>
+          {/* <Link href="/product/ID"> */}
+          <button type="button">{buttonText}</button>
+          {/* </Link> */}
 
-          <div className="desc">
-            {/* <h5>Description:</h5> */}
-            <p>{heroBanner.desc}</p>
-          </div>
+          <div className="desc"></div>
         </div>
       </div>
     </div>
